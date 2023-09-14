c875b4d8da23f6dbfaaa17610e2623401c7588e3
Roland Labrador <rolando1515@hotmail.com>
2023-09-14 10:12:14 -0400

    commit

--- src/ui/shared/Footer /footer_styles.tsx
+++ src/ui/shared/Footer /footer_styles.tsx
@@ -1,1 +1,19 @@
 import styled from "styled-components";
+
+export const Style = {
+    Container:styled.footer`
+        width: Hug (1,080px);
+        height: Hug (49px);
+        top: 5073px;
+        left: 180px;
+        gap: 117px;
+        background-color: #FFFFFF;
+
+
+
+
+
+
+
+        `,
+};
