0fc846b530102a64485420a4cc9ce16a4268cb9f
Roland Labrador <rolando1515@hotmail.com>
2023-09-28 16:30:42 -0400

    commit

--- src/ui/Components/ProjectCard/projectCard.tsx
+++ src/ui/Components/ProjectCard/projectCard.tsx
@@ -2,15 +2,40 @@ import { Github, Link as LinkIcon } from '../Icons';
 import { Style } from './styles';
 
 export function ProjectCard({
-    title,
-    detail,
-    image,
-    link,
-    github,
+  title,
+  detail,
+  image,
+  link,
+  github,
 }: {
-    title: string;
-    detail: string;
-    
+  title: string;
+  detail: string;
+  image: string;
+  link: string;
+  github: string;
+}) {
+  return (
+    <Style.Container image={image}>
+      <figure></figure>
+      <hgroup>
+        <h3>{title}</h3>
+        <div>
+          <a href={github} target="_blank">
+            <Github />
+          </a>
+          <a href={link} target="_blank">
+            <LinkIcon />
+          </a>
+        </div>
+      </hgroup>
+
+      <p>{detail}</p>
+    </Style.Container>
+  );
+}
+
+
+
 
 
 
